1b3a1e759edcfff39c74b88c5b116375586d1ff7
bdog931 <brandonharrell1999@gmail.com>
2021-05-31 09:23:09 -0500

    Checkerboard now has pieces on it

--- Games/Checkers/checkers.js
+++ Games/Checkers/checkers.js
@@ -1,31 +1,52 @@
-const width = 8;
-const height = 8;
+let width = 8;
+let height = 8;
 
-let board = new Array(width);
 
-for(let i=0;i<width; i++){
-    board[i] = new Array(height);
+//Creating 2D Array
+let rows = new Array(height);
+for(let i=0; i<height; i++){
+    rows[i] = new Array(width);
 }
 
-for(let i=0; i<width; i++){
-    for(let j=0; j<height; j++){
-        board[i][j] = i + j;
-        let gridBackdrop = document.createElement("td");
-        let gridPiece = document.createElement("DIV");
-        if((i + j) % 2 == 0){
-           gridPiece.className = "dark-stones"; 
+//Initializing 2D Array
+for(let i=0; i<height; i++){
+    for(let j=0; j<width; j++){
+        console.log((i * 8) + j + 1);
+        rows[i][j] = (i * 8) + j + 1;
+        let newPiece = document.createElement('div');
+        if((i + j) % 2 === 0){
+            newPiece.className = "white-tile";
+            newPiece.id = (i * 8) + j + 1;
         }
-        if((i + j) % 2 == 1){
-            gridPiece.className = "lighter-stones";
+        if((i + j) % 2 === 1){
+            newPiece.className = "black-tile";
+            newPiece.id = (i * 8) + j + 1;
         }
-        console.log(i + j + ", ");
+        document.getElementById("board-container").appendChild(newPiece);
     }
 }
 
-let hey = "Hello";
+//Putting Chips on relevant tiles
+let setupGame = () => {
+    for(let i=0; i<width; i++){
+        for(let j=0; j<height; j++){
+            if(i <= 2){
+                if((i + j) % 2 === 1){
+                    let newChip = document.createElement('div');
+                    newChip.className = "golden-piece";
+                    document.getElementById((i * 8) + j + 1).appendChild(newChip);
+                }
+            }
+            if(i >= 5){
+                if((i + j) % 2 === 1){
+                    let newChip = document.createElement('div');
+                    newChip.className = "purple-piece";
+                    document.getElementById((i * 8) + j + 1).appendChild(newChip);
+                }
+            }
+        }
+    }
+}
 
-document.getElementById("counter").addEventListener("click", function(){
-    document.getElementById("counter").innterHTML = hey;
-    console.log("I have been clicked");
-})
-
+//Set up game
+document.getElementById("playGame").addEventListener("click", setupGame);
